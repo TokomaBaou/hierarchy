c6bc54567bce849797567bc5540caee634989775
Macart <86038701+TokomaBaou@users.noreply.github.com>
2022-07-21 22:53:59 +0900

    Promise

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,24 @@ export default function App() {
     });
   };
 
+  type User = {
+    name: string;
+    age: number;
+  };
+  function getUser(): Promise<User> {
+    return new Promise((resolve) => {
+      const user: User = {
+        name: "太郎",
+        age: 10
+      };
+      resolve(user);
+    });
+  }
+  getUser().then((user: User) => {
+    console.log(user);
+    // @log: { "name": "太郎", "age": 10 }
+  });
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
